6207cd8cf05af65091af984231b3ac2f920b02be
khokharmosin <khokharmosin7806@gmail.com>
2022-03-23 10:54:57 +0530

    MODIFIED: Display Multiple Contacts

--- src/com/infogalaxy/AddressBook.java
+++ src/com/infogalaxy/AddressBook.java
@@ -1,11 +1,14 @@
 package com.infogalaxy;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AddressBook {
     Scanner sc = new Scanner(System.in);
-    Contact contact = new Contact();
-    public void getcontact(){
+    ArrayList<Contact> contactlist = new ArrayList<>();
+
+    public void getcontact() {
+        Contact contact = new Contact();
         System.out.println("Enter The First Name ; ");
         contact.setFirstName(sc.next());
         System.out.println("Enter The Last Name : ");
@@ -22,13 +25,33 @@ public class AddressBook {
         contact.setEmail(sc.next());
         System.out.println("Enter The Zip Code : ");
         contact.setZipCode(sc.next());
+        contactlist.add(contact);
     }
-    public void showcontact(){
-        System.out.println(contact.toString());
+
+    public void showcontact() {
+        for (int i = 0; i < contactlist.size(); i++) {
+            Contact contact = contactlist.get(i);
+            System.out.println(contact.toString());
+        }
     }
+
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         AddressBook addressBook = new AddressBook();
-        addressBook.getcontact();
-        addressBook.showcontact();
+        int choice;
+        do {
+            System.out.println("***ADDRESS BOOK MANAGER***");
+            System.out.println("\n1. ADD CONTACT \n2. DISPLAY CONTACT \n3. EXIT");
+            System.out.println("Enter Your Choice :");
+            choice = sc.nextInt();
+            switch (choice) {
+                case 1:
+                    addressBook.getcontact();
+                    break;
+                case 2:
+                    addressBook.showcontact();
+                    break;
+            }
+        } while (choice != 3);
     }
 }
